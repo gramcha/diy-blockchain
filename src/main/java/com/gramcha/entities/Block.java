beb886171dc41bfae5209f8097e3a54f8d9692d0
Ganeshkumar Ramachandran <gramachandran@Ganeshkumars-MacBook-Pro.local>
2017-12-26 18:32:57 +0530

    type change of transaction data

--- src/main/java/com/gramcha/entities/Block.java
+++ src/main/java/com/gramcha/entities/Block.java
@@ -13,7 +13,7 @@ public class Block {
 
 	Integer id;
 	String previousBlockHash;
-	Object transactionData;
+	String transactionData;
 	String timeStamp;
 	String blockHash;
 	/**
@@ -23,19 +23,26 @@ public class Block {
 	 * @param timeStamp
 	 * @throws NoSuchAlgorithmException 
 	 */
-	public Block(Integer id, String previousBlockHash, Object transactionData, String timeStamp) throws NoSuchAlgorithmException {
+	public Block() {
+		
+	}
+	public Block(Integer id, String previousBlockHash, String transactionData, String timeStamp) throws NoSuchAlgorithmException {
 		super();
 		this.id = id;
 		this.previousBlockHash = previousBlockHash;
 		this.transactionData = transactionData;
 		this.timeStamp = timeStamp;
-		
+		System.out.println("#####");
 		this.blockHash = calculateBlockHash(id, previousBlockHash, transactionData, timeStamp);
+		System.out.println("#####");
 	}
-	private static String calculateBlockHash(Integer id, String previousBlockHash, Object transactionData, String timeStamp)
+	public static String calculateBlockHash(Integer id, String previousBlockHash, String transactionData, String timeStamp)
 			throws NoSuchAlgorithmException {
 		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		byte[] hash = digest.digest((id+previousBlockHash+transactionData+timeStamp).getBytes());
+//		byte[] hash = digest.digest((id+previousBlockHash+transactionData+timeStamp).getBytes());
+		String hashInput = id+previousBlockHash+transactionData+timeStamp;
+		System.out.println("hashInput = "+hashInput);
+		byte[] hash = digest.digest(hashInput.getBytes());
 		return Base64.getEncoder().encodeToString(hash);
 	}
 	public static String calculateHash(Block block) throws NoSuchAlgorithmException {
@@ -47,7 +54,7 @@ public class Block {
 	public String getPreviousBlockHash() {
 		return previousBlockHash;
 	}
-	public Object getTransactionData() {
+	public String getTransactionData() {
 		return transactionData;
 	}
 	public String getTimeStamp() {
@@ -72,36 +79,44 @@ public class Block {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
+//		System.out.println("this check");
 		if (obj == null)
 			return false;
+//		System.out.println("obj check");
 		if (getClass() != obj.getClass())
 			return false;
+//		System.out.println("starting check");
 		Block other = (Block) obj;
 		if (blockHash == null) {
 			if (other.blockHash != null)
 				return false;
 		} else if (!blockHash.equals(other.blockHash))
 			return false;
+//		System.out.println("blockHash check");
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+//		System.out.println("id check");
 		if (previousBlockHash == null) {
 			if (other.previousBlockHash != null)
 				return false;
 		} else if (!previousBlockHash.equals(other.previousBlockHash))
 			return false;
+//		System.out.println("previousBlockHash check");
 		if (timeStamp == null) {
 			if (other.timeStamp != null)
 				return false;
 		} else if (!timeStamp.equals(other.timeStamp))
 			return false;
+//		System.out.println("timeStamp check");
 		if (transactionData == null) {
 			if (other.transactionData != null)
 				return false;
 		} else if (!transactionData.equals(other.transactionData))
 			return false;
+//		System.out.println("transactionData check");
 		return true;
 	}
 	@Override
